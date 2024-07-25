a7e73b06ac26405a7d7f7eaa9df4b62ad47ce23b
Murtadha Ahmed <38778295+amurtadha@users.noreply.github.com>
2024-07-25 16:48:05 +0800

    Update MyModel.py

--- MyModel.py
+++ MyModel.py
@@ -5,6 +5,28 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 from torch.autograd import Function
+class Vanilla(nn.Module):
+    def __init__(self, args, hidden_size=256):
+        super(Vanilla, self).__init__()
+        config = AutoConfig.from_pretrained(args.pretrained_bert_name)
+        self.encoder = AutoModel.from_pretrained(args.pretrained_bert_name, config=config)
+        self.encoder.to('cuda')
+
+        layers = [nn.Linear(config.hidden_size, args.lebel_dim)]
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+
+        self.classifier = nn.Sequential(*layers)
+
+    def forward(self, inputs):
+
+        input_ids,token_type_ids, attention_mask = inputs[:3]
+        outputs = self.encoder(input_ids, token_type_ids=token_type_ids, attention_mask=attention_mask)
+        # pooled_output = outputs['last_hidden_state'][:, 0, :]
+        pooled_output = outputs[-1]
+        pooled_output = self.dropout(pooled_output)
+
+
+        return  self.classifier(pooled_output)
 
 class GradientReverseLayer(Function):
     @staticmethod
